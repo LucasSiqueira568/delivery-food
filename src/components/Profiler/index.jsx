a4e4dc19629e58aa0c7d9a6d762dcaa43cb251e7
LucasSiqueira568 <siqueiralucas-pro@outlook.com>
2021-09-22 00:29:26 -0300

    Correção na roda do profiler

--- src/components/Profiler/index.jsx
+++ src/components/Profiler/index.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Image, ScrollView, View, TouchableOpacity } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import styles from './style'
-import { SimpleLineIcons } from '@expo/vector-icons';
-import ListProfiler from '../pages/ListProfiler'
-
-
+import React from "react";
+import { Image, ScrollView, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import styles from "./style";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import ListProfiler from "../pages/ListProfiler";
 
 export default function ProfilerUser(props) {
   return (
     <ScrollView>
-
       <View style={styles.container}>
-        <View style={{
-          flex: 1,
-          padding: 150,
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'relative',
-        }}>
+        <View
+          style={{
+            flex: 1,
+            padding: 150,
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}
+        >
           <View>
             <Image
               style={{
@@ -26,27 +25,28 @@ export default function ProfilerUser(props) {
                 height: 150,
                 padding: 10,
               }}
-              source={require('../../assets/user.png')}
+              source={require("../../assets/user.png")}
             />
-            <TouchableOpacity  style={{
-              position: 'absolute',
-              left: 120,
-              top: 30,
-              backgroundColor: 'orange',
-              width: 40,
-              height: 40,
-              borderRadius: 40,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+            <TouchableOpacity
+              style={{
+                position: "absolute",
+                left: 120,
+                top: 30,
+                backgroundColor: "orange",
+                width: 40,
+                height: 40,
+                borderRadius: 40,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <SimpleLineIcons name="camera" size={22} color="white" />
             </TouchableOpacity>
-
           </View>
           <TouchableOpacity
             onPress={() => props.navigation.navigate("Main")}
             style={{
-              position: 'absolute',
+              position: "absolute",
               top: 35,
               left: 15,
             }}
@@ -56,63 +56,62 @@ export default function ProfilerUser(props) {
         </View>
         <View>
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Lucas Siqueira Rodrigues"
             nameIcon="user"
             rightIcon="pencil"
             onPress={() => console.warn("Clicou")}
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Senha"
             nameIcon="lock"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="E-mail"
             nameIcon="mail"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Telefone"
             nameIcon="phone"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Endereço"
             nameIcon="map"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Cartões"
             nameIcon="credit-card"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Ajuda"
             nameIcon="info"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Configurações"
             nameIcon="settings"
             rightIcon="arrow-right"
           />
           <ListProfiler
-            url={require('../../assets/hamburguer_churrasco.jpg')}
+            url={require("../../assets/hamburguer_churrasco.jpg")}
             title="Termo de Uso"
             nameIcon="info"
             rightIcon="arrow-right"
           />
         </View>
       </View>
-
     </ScrollView>
   );
 }
